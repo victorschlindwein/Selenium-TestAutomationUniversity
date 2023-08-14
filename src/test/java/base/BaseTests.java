4eb358df95b05b3b06af18a29e48648521c38eaf
dizzysch <victorwilbert@gmail.com>
2023-08-14 09:52:31 -0300

    ex1 done

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,6 +1,9 @@
 package base;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTests {
@@ -9,13 +12,25 @@ public class BaseTests {
     public void setUp(){
         System.setProperty("webdriver.chrome.diver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://www.google.com.br");
+        driver.get("https://the-internet.herokuapp.com/");
+
+        driver.manage().window().setSize(new Dimension(1024, 812));
+
+        WebElement shifting_Content = driver.findElement(By.linkText("Shifting Content"));
+        shifting_Content.click();
+
         System.out.println(driver.getTitle());
-        driver.quit();
+//        driver.quit();
+    }
+
+    public void clickOnShiftContent(){
+        WebElement menu_Element = driver.findElement(By.linkText("Example 1: Menu Element"));
+        menu_Element.click();
     }
 
     public static void main(String[] args) {
         BaseTests tests = new BaseTests();
         tests.setUp();
+        tests.clickOnShiftContent();
     }
 }
