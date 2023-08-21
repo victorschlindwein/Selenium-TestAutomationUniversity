50a628da75b41368e645ec84799f5ff63fb68a23
dizzysch <victorwilbert@gmail.com>
2023-08-21 13:18:27 -0300

    add event reporter

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManager;
 
 import java.io.File;
@@ -18,14 +20,15 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTests {
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
         //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.register(new EventReporter());
         goHome();
         homePage = new HomePage(driver);
     }
