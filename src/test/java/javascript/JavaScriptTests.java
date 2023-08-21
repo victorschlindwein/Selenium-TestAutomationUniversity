c03de5453845f701001ec4fd30fb79190db95ce4
dizzysch <victorwilbert@gmail.com>
2023-08-21 10:09:02 -0300

    add javascript tests

---
+++ src/test/java/javascript/JavaScriptTests.java
@@ -0,0 +1,16 @@
+package javascript;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+public class JavaScriptTests extends BaseTests {
+    @Test
+    public void testScrollTable(){
+        homePage.clickLargeAndDeepDom().scrollToTable();
+    }
+
+    @Test
+    public void testScrollToFifthParagraph(){
+        homePage.clickInfiniteScroll().scrollToParagraph(5);
+    }
+}
